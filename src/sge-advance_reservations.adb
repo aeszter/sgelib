0a63efabed9bbb9b1af52953b780c68f1c97083e
Ansgar Esztermann <aeszter@gwdg.de>
2014-04-30 10:51:38 +0200

    Finish AR implementation (Bug #1997)

--- src/sge-advance_reservations.adb
+++ src/sge-advance_reservations.adb
@@ -12,16 +12,24 @@ package body SGE.Advance_Reservations is
    -----------------
    procedure Update_State_Array (R : in out Reservation);
    procedure Extract_Resource_List (R              : in out Reservation;
-                                    Resource_Nodes : Node_List;
-                                    Soft           : Boolean := False);
+                                    Resource_Nodes : Node_List);
    procedure Extract_Slots_List (R : in out Reservation; Children : Node_List);
 
    procedure Extract_Resource_List (R              : in out Reservation;
-                                    Resource_Nodes : Node_List;
-                                    Soft           : Boolean := False) is
-      pragma Unreferenced (R, Resource_Nodes, Soft);
+                                    Resource_Nodes : Node_List) is
+      Entry_Node : Node;
+      A          : Attr;
+      Res_Value : Unbounded_String;
    begin
-      Ada.Text_IO.Put_Line ("Bug #2018: Extract_Resource_List unimplemented");
+      for I in 0 .. Length (Resource_Nodes) - 1 loop
+         Entry_Node := Item (Resource_Nodes, I);
+         if Name (Entry_Node) = "resource" then
+            A := Get_Attr (Entry_Node, "type");
+            Res_Value := To_Unbounded_String (Value (A));
+            A := Get_Attr (Entry_Node, "name");
+            R.Add_Resource (Key => Value (A), Value => To_String (Res_Value));
+         end if;
+      end loop;
    end Extract_Resource_List;
 
    procedure Append_List (Node_Tree : SGE.Parser.Tree) is
@@ -38,8 +46,22 @@ package body SGE.Advance_Reservations is
    end Append_List;
 
    function New_Reservation (List : SGE.Parser.Node_List) return Reservation is
+      function HMS_To_Seconds (S : String) return Natural is
+         Separator, Start : Positive;
+         Seconds : Positive;
+      begin
+         Separator := Index (Source => S,
+                             Pattern => ":");
+         Seconds := 3_600 * Integer'Value (S (S'First .. Separator - 1));
+         Start := Separator + 1;
+         Separator := Index (Source => S (Start .. S'Last),
+                             Pattern => ":");
+         Seconds := Seconds + 60 * Integer'Value (S (Start .. Separator - 1));
+         Seconds := Seconds + Integer'Value (S (Separator + 1 .. S'Last));
+         return Seconds;
+      end HMS_To_Seconds;
+
       C : Node;
-      A : Attr;
       R : Reservation;
    begin
       for Index in 0 .. Length (List) - 1 loop
@@ -63,7 +85,7 @@ package body SGE.Advance_Reservations is
             elsif Name (C) = "duration" then
                R.Time_Span := Ada.Real_Time.To_Duration
                  (Ada.Real_Time.Seconds
-                    (Integer'Value (Value (First_Child (C)))));
+                    (HMS_To_Seconds (Value (First_Child (C)))));
             elsif Name (C) = "submission_time" then
                R.Submission_Time := To_Time (Value (First_Child (C)));
             elsif Name (C) = "group" then
@@ -110,13 +132,8 @@ package body SGE.Advance_Reservations is
 
    function Get_State (R : Reservation) return String is
    begin
-      return R.State_String;
+      return Ada.Strings.Fixed.Trim (R.State_String, Ada.Strings.Right);
    end Get_State;
-
-   function Get_Start (R : Reservation) return Ada.Calendar.Time is
-   begin
-      return R.Start_Time;
-   end Get_Start;
 
    function Get_Duration (R : Reservation) return Duration is
    begin
@@ -138,6 +155,17 @@ package body SGE.Advance_Reservations is
    begin
       R.Error_Log.Iterate (Wrapper'Access);
    end Iterate_Error_Log;
+
+   procedure Iterate_Queues (R       : Reservation;
+                             Process : not null access procedure (Q : Queue)) is
+      procedure Wrapper (Position : Queue_Lists.Cursor) is
+      begin
+         Process (Queue_Lists.Element (Position));
+      end Wrapper;
+
+   begin
+      R.Queues.Iterate (Wrapper'Access);
+   end Iterate_Queues;
 
    procedure Iterate (Process : not null access procedure (R : Reservation)) is
       procedure Wrapper (Position : AR_Lists.Cursor) is
@@ -205,10 +233,80 @@ package body SGE.Advance_Reservations is
    end Update_State_Array;
 
    procedure Extract_Slots_List (R : in out Reservation; Children : Node_List) is
-      pragma Unreferenced (R, Children);
+      Slot_Node : Node;
+      A         : Attr;
+      Slots : Positive;
    begin
-      Ada.Text_IO.Put_Line ("Extract_Slots_List unimplemented");
+      for I in 0 .. Length (Children) - 1 loop
+         Slot_Node := Item (Children, I);
+         if Name (Slot_Node) = "granted_slots" then
+            A := Get_Attr (Slot_Node, "slots");
+            Slots := Integer'Value (Value (A));
+            A := Get_Attr (Slot_Node, "queue_instance");
+            R.Append_Queue (Q => Value (A), Slots => Slots);
+         end if;
+      end loop;
    end Extract_Slots_List;
+
+   procedure Append_Queue (R : in out Reservation; Q : String; Slots : Positive) is
+      New_Item : Queue := (Name => To_Unbounded_String (Q), Slots => Slots);
+   begin
+      R.Queues.Include (Key      => To_Key (Q),
+                        New_Item => New_Item);
+   end Append_Queue;
+
+   procedure Add_Resource (R : in out Reservation; Key, Value : String) is
+   begin
+      R.Resource_List.Insert (Key      => To_Unbounded_String (Key),
+                              New_Item => Resources.New_Resource (Name  => Key,
+                                                                  Value => Value));
+   end Add_Resource;
+
+   function To_Key (S : String) return Queue_Key is
+   begin
+      return Queue_Key (Queue_Keys.To_Bounded_String (Source => S,
+                                                      Drop   => Ada.Strings.Right));
+   end To_Key;
+
+   function Get_Group (R : Reservation) return String is
+   begin
+      return To_String (R.Group);
+   end Get_Group;
+
+   function Get_Account (R : Reservation) return String is
+   begin
+      return To_String (R.Account);
+   end Get_Account;
+
+   function Get_Start_Time (R : Reservation) return Ada.Calendar.Time is
+   begin
+      return R.Start_Time;
+   end Get_Start_Time;
+
+   function Get_End_Time (R : Reservation) return Ada.Calendar.Time is
+   begin
+      return R.End_Time;
+   end Get_End_Time;
+
+   function Get_Submission_Time (R : Reservation) return Ada.Calendar.Time is
+   begin
+      return R.Submission_Time;
+   end Get_Submission_Time;
+
+   function Get_Resources (R : Reservation) return Resources.Hashed_List is
+   begin
+      return R.Resource_List;
+   end Get_Resources;
+
+   function Get_Name (Q : Queue) return String is
+   begin
+      return To_String (Q.Name);
+   end Get_Name;
+
+   function Get_Slots (Q : Queue) return String is
+   begin
+      return Q.Slots'Img;
+   end Get_Slots;
 
 
 end SGE.Advance_Reservations;
--- src/sge-advance_reservations.ads
+++ src/sge-advance_reservations.ads
@@ -4,10 +4,17 @@ with SGE.Parser;
 with SGE.Utils; use SGE.Utils;
 with SGE.Jobs; use SGE.Jobs;
 with Ada.Containers.Doubly_Linked_Lists;
+with Ada.Strings.Bounded;
+with Ada.Containers.Ordered_Maps;
+with SGE.Resources;
 
 package SGE.Advance_Reservations is
    Other_Error : exception;
    type Reservation is private;
+   type Queue is private;
+
+   function Get_Name (Q : Queue) return String;
+   function Get_Slots (Q : Queue) return String;
 
    procedure Append_List (Node_Tree : SGE.Parser.Tree);
 
@@ -15,9 +22,17 @@ package SGE.Advance_Reservations is
    function Get_Name (R : Reservation) return String;
    function Get_ID (R : Reservation) return String;
    function Get_Owner (R : Reservation) return User_Name;
+   function Get_Group (R : Reservation) return String;
+   function Get_Account (R : Reservation) return String;
    function Get_State (R : Reservation) return String;
-   function Get_Start (R : Reservation) return Ada.Calendar.Time;
+   function Get_Start_Time (R : Reservation) return Ada.Calendar.Time;
+   function Get_End_Time (R : Reservation) return Ada.Calendar.Time;
+   function Get_Submission_Time (R : Reservation) return Ada.Calendar.Time;
    function Get_Duration (R : Reservation) return Duration;
+   function Get_Resources (R : Reservation) return Resources.Hashed_List;
+
+   procedure Iterate_Queues (R       : Reservation;
+                             Process : not null access procedure (Q : Queue));
 
    function Has_Error_Log_Entries (R : Reservation) return Boolean;
    procedure Iterate_Error_Log (R       : Reservation;
@@ -25,9 +40,28 @@ package SGE.Advance_Reservations is
    procedure Iterate (Process : not null access procedure (R : Reservation));
    function Same (Left, Right : Reservation) return Boolean;
 
-
+   procedure Append_Queue (R : in out Reservation; Q : String; Slots : Positive);
+   procedure Add_Resource (R : in out Reservation; Key, Value : String);
 private
-   type Reservation is record
+   Key_Length : constant Positive := 16;
+   package Queue_Keys is
+     new Ada.Strings.Bounded.Generic_Bounded_Length (Max => Key_Length);
+   type Queue_Key is new Queue_Keys.Bounded_String;
+   type Queue is record
+      Name : Unbounded_String;
+      Slots : Positive;
+   end record;
+
+   function To_Key (S : String) return Queue_Key;
+
+   procedure Record_Error (R : in out Reservation; Message : String);
+   --  Purpose: store an error message for retrieval by the calling application
+   --  without raising an exception (so we can resume Library oprations)
+
+   package Queue_Lists is
+      new Ada.Containers.Ordered_Maps (Key_Type => Queue_Key, Element_Type => Queue);
+
+   type Reservation is tagged record
       Number               : Integer; -- Job ID
       Name                 : Unbounded_String; -- Job name
       Owner                : Utils.User_Name; -- User whom this job belongs to
@@ -38,6 +72,8 @@ private
       Start_Time, End_Time : Time;
       Time_Span            : Duration;
       Submission_Time      : Time;    -- when submitted
+      Queues               : Queue_Lists.Map;
+      Resource_List        : Resources.Hashed_List;
       Error_Log            : Utils.String_List;
 
    end record;
@@ -47,8 +83,5 @@ private
 
    List : AR_Lists.List;
 
-   procedure Record_Error (R : in out Reservation; Message : String);
-   --  Purpose: store an error message for retrieval by the calling application
-   --  without raising an exception (so we can resume Library oprations)
 
 end SGE.Advance_Reservations;
