99c79f4d3bed7e483d24617d98fc6d90bfec985a
Ansgar Esztermann <aeszter@gwdg.de>
2014-12-05 16:07:33 +0100

    Logger type: add global log
    Keeping an error log not tied to individual objects is useful e.g. when the object at hand is read-only.

--- src/sge-loggers.ads
+++ src/sge-loggers.ads
@@ -1,10 +1,14 @@
 with SGE.Utils;
 package SGE.Loggers is
    type Logger is tagged private;
+
    function Has_Errors (Object : Logger) return Boolean;
+   function Errors_Exist return Boolean;
    procedure Record_Error (Object : in out Logger; Message : String);
+   procedure Record_Error (Message : String);
    procedure Iterate_Errors (Object  : Logger;
                              Process : not null access procedure (Message : String));
+   procedure Iterate_Errors (Process : not null access procedure (Message : String));
 
 private
    type Logger is tagged record
