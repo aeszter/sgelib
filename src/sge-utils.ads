6edce46d249e8cf2b15fbf2cb533c87ed286ead7
Ansgar Esztermann <aeszter@gwdg.de>
2015-03-04 10:44:53 +0100

    pre-release version bump

--- src/sge-utils.ads
+++ src/sge-utils.ads
@@ -7,7 +7,7 @@ with POSIX; use POSIX;
 with Ada.Calendar;
 
 package SGE.Utils is
-   Version : String := "develop";
+   Version : String := "v0.17";
    type Tri_State is (False, True, Undecided);
    type User_Name is new String (1 .. 8);
 
