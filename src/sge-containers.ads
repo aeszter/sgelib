ce7bfb73729ad8e05207ce892e8d3e06a746d4dd
Ansgar Esztermann <aeszter@gwdg.de>
2015-10-30 16:06:29 +0100

    Containers (Bug #2408): make generic
    According to http://patchwork.ozlabs.org/patch/112008/, it should be possible to instantiate this publicly for private Element_Type, but I have not managed to do so.
    
    Does not compile.

--- src/sge-containers.ads
+++ src/sge-containers.ads
@@ -1,11 +1,15 @@
 with ada.Iterator_Interfaces;
 
+generic
+   type Element_Type (<>) is tagged private;
 package SGE.Containers is
    type Container is abstract tagged null record;
 
    function Length (Collection:Container) return Natural is abstract;
    procedure Clear (Collection : in out Container) is abstract;
    function Is_Empty (Collection: Container) return Boolean is abstract;
+   procedure Iterate (Collection: Container;
+                      Process   : not null access procedure (Item : aliased Element_Type));
 
 
 
